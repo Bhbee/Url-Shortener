3083d8ab8aa09648a4383008c88246d83cf8506b
Bhbee <samuelchristiana38@gmail.com>
2023-02-03 14:22:26 +0100

    Made short-url customizable by user

--- server.js
+++ server.js
@@ -16,19 +16,23 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
 
-  res.redirect('/')
+  const checkShortUrl = req.body.shortUrl;
+  const ShortUrlExists = await ShortUrl.findOne({short: checkShortUrl }).exec();
+  if (ShortUrlExists) { 
+    return res.status(409).json({"message": "Customized shortUrl already in use"})//conflict
+  }
+  else {
+    await ShortUrl.create({ full: req.body.fullUrl, short: req.body.shortUrl })
+    res.redirect('/')
+  }; 
 })
 
 app.get('/:shortUrl', async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
   if (shortUrl == null) return res.sendStatus(404)
 
-  shortUrl.clicks++
-  shortUrl.save()
-
   res.redirect(shortUrl.full)
 })
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
